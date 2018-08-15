20484027180056156783182792e7eb4b1ecf5f91
Brandon Lam <brandonywlam@gmail.com>
2018-08-14 23:38:35 -0700

    rewriting behaviour
    - add createCondition method
    - add _normalizeTree method
    - remove bad check in createPolicy method
    - add helper functions
    - add conditions property
    - remove null type declaration for instances of subject

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,37 @@
 enum Defaults {
   NULL_SUBJECT = 'NULL_SUBJECT'
 }
+
+function validSubject<TS>(subject:Constructor<TS>|AnyObject) {
+  return typeof subject === 'object' || typeof subject === 'function';
+};
+function validResource<TR>(resource:Constructor<TR>|AnyObject) {
+  return typeof resource === 'object' || typeof resource === 'function';
+};
+function shiftNodeFromArray(nodes:{[any:string]:any}[]) {
+  nodes.shift();
+  return nodes;
+}
+function isPlainObject(obj:object) {
+  return obj === Object(obj) && !Array.isArray(obj) && typeof obj !== 'function';
+}
 export default class AbacApe {
   policies: {[key:string]:{[key:string]:{[key:string]:CreatePolicyOptions<any,any>['policy']}}};
+  conditions: {[key:string]:{[key:string]:{[key:string]:<TS,TR>(subject:InstanceType<Constructor<TS>>, resource:InstanceType<Constructor<TR>>, environment:any) => boolean|Error}}};
   createError: any;
   constructor() {
     this.policies = {};
+    this.conditions = {};
   }
 
   createPolicy<TS, TR>({subject, action, resource, environment, policy}:CreatePolicyOptions<TS, TR>) {
+    // make sure is array or string
     if (!( (Array.isArray(action) || (typeof action === 'string') ))) {
       throw new TypeError(`Expected action to be string or array of strings, got ${typeof action}`)
     }
+    
     if(typeof policy !== 'function') {
       throw new TypeError(`Expected policy to be function, got ${typeof policy}`);
-    }
-    if(typeof subject === 'undefined') {
-      throw new TypeError(`Expected subject to bo object or null, got ${typeof subject}`);
     }
 
     if (typeof action === 'string') {
@@ -84,39 +99,104 @@ export default class AbacApe {
     }
   }
 
-  checkPolicy<TSubjectClassOrObject,TResourceClassOrObject>({subject, action, resource, environment}:AuthorizeOptions<TSubjectClassOrObject,TResourceClassOrObject>) :PolicyResultsObject{
-    if (subject) {
-      try {
-        return this.policies[resource.constructor.name][subject.constructor.name][action](subject,resource,environment);
-      } catch (error) {
-        let errors = [];
-        // TODO: make error into a
-        errors.push(new ReferenceError(`Subject:${subject.constructor.name} Action:${action} Resource:${resource.constructor.name} is not a policy`))
-        return {result:false, errors}
+  createCondition<TS, TR>({subject, resource, environment, condition}:any) {
+    if(!validSubject(subject)) {
+      throw new TypeError(`Expected subject to be constructor or plain object, got ${typeof subject}`);
+    };
+    if(!validResource(resource)) {
+      throw new TypeError(`Expected resource to be constructor or plain object, got ${typeof resource}`);
+    }
+
+    for (const key in condition) {
+      if (condition.hasOwnProperty(key)) {
+        const statement = condition[key];
+        this._addCondition(subject, resource, environment, key, statement[key]);
       }
-    } else {
-      try {
-        return this.policies[resource.constructor.name][Defaults.NULL_SUBJECT][action](subject, resource, environment);
-      } catch (error) {
-        let errors = [];
-        // should push error into errors array.
-        errors.push(new ReferenceError(`Subject:null Action:${action} Resource:${resource.constructor.name} is not a policy`))
-        return {result: false, errors};
-      }
+    }
+  }
+
+  checkPolicy<TSubjectClassOrObject, TResourceClassOrObject>({subject, action, resource, environment}:AuthorizeOptions<TSubjectClassOrObject,TResourceClassOrObject>) :PolicyResultsObject{
+    try {
+      return this.policies[subject.constructor.name][action][resource.constructor.name](subject, resource, environment);
+    } catch (error) {
+      let errors = [];
+      // TODO: make error into a
+      errors.push(new ReferenceError(`Subject:${subject.constructor.name} Action:${action} Resource:${resource.constructor.name} is not a policy`))
+      return {result:false, errors}
     }
   }
 
   printPolicies() {
     console.dir(this.policies);
   }
+
+  auth(subject:any) {
+    return (action:any) => {
+      return (resource:any) => {
+        return this.checkPolicy({
+          subject,
+          action,
+          resource
+        })
+      }
+    }
+  }
+
+  private _addCondition<TS, TR>(subject:Constructor<TS>, resource:Constructor<TR>, environment:any, key:string, func:any) {
+    this._normalizeTree(this.conditions,[ subject, resource]);
+    this.conditions[subject.name][resource.name][key] = func;
+  }
+
+  private _normalizeTree(tree:{[any:string]:any}, nodes:{[any:string]:any}[] | Constructor<{[any:string]:any}>[]) {
+    for (let i = 0; i < nodes.length; i++) {
+      let node_name = nodes[i].name;
+      if (tree.hasOwnProperty(node_name)) {
+        this._normalizeTree(tree[node_name], shiftNodeFromArray(nodes))
+      } else {
+        tree[node_name] = {};
+        this._normalizeTree(tree[node_name], shiftNodeFromArray(nodes))
+      }
+    }
+  };
 }
 
-interface CreatePolicyOptions<TSubjectClassOrObject, TResourceClassOrObject = null> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// types
+interface CreatePolicyOptions<TSubject, TResource> {
   /**
    * Used to index the constructor of the subject.
    * The subject parameter in the policy function must be an instance of this Object/Constructor
    */
-  subject: Constructor<TSubjectClassOrObject> | AnyObject;
+  subject: Constructor<TSubject> | AnyObject;
   /**
    * Name of action being performed.
    */
@@ -125,32 +205,19 @@ interface CreatePolicyOptions<TSubjectClassOrObject, TResourceClassOrObject = nu
    * Used to index the constructor of the resource.
    * The resource parameter in the policy function must be an instance of this Object/Constructor
    */
-  resource: Constructor<TResourceClassOrObject> | AnyObject;
+  resource: Constructor<TResource> | AnyObject;
   /**
    * Additional data used to authenticate the policy.
    */
   environment?: any;
+
   /**
-   * @description Creates a Policy. A Policy should be made up of one or more conditions and should always
-   * return 
-   * @example
    * 
-   * policy(subject, resource, environment) {
-   *  let result = {
-   *    result: false,
-   *    errors: []
-   *  };
-   *  condition(result);
-   *  return result;
-   * }
-   * @example
-   * 
-   * @param subject Instance of Object requesting resource.
-   * @param resource Instnace of Object being requested.
-   * @param environment Environment and additional data used to authorize access to the reqested resource.
-   * @see createPolicy
+   * @param subject Instance of Subject requesting Resource
+   * @param resource Instance of Resource being requested
+   * @param environment Additional data used by conditions to judge policy verdict
    */
-  policy(subject:InstanceType<Constructor<TSubjectClassOrObject>> | AnyObject, resource: InstanceType<Constructor<TResourceClassOrObject>> | AnyObject, environment:any):PolicyResultsObject;
+  policy<TSubject, TResource>(subject:InstanceType<Constructor<TSubject>> | AnyObject, resource:InstanceType<Constructor<TResource>> | AnyObject, environment:any) :PolicyResultsObject;
 }
 
 type AnyObject = {[any:string]: any};
@@ -161,7 +228,7 @@ interface PolicyResultsObject {
 }
 
 interface AuthorizeOptions<TSubjectClassOrObject, TResourceClassOrObject> {
-  subject: InstanceType<Constructor<TSubjectClassOrObject>> | null;
+  subject: InstanceType<Constructor<TSubjectClassOrObject>>;
   resource:InstanceType<Constructor<TResourceClassOrObject>>;
   environment?:any;
   action:string;
